93682b24bf27e78c2aac59926500ccffa0114829
jianzhiwang <1125246519@qq.com>
2019-10-23 05:49:13 +0800

    测试linux操作系统的网速

---
+++ ceshi.sh
@@ -0,0 +1,6 @@
+# wget https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+# 说明:资源上传
+# url参考:https://www.cnblogs.com/KevinJasmine/p/6700312.html
+chmod a+rx speedtest.py
+mv speedtest.py /usr/local/bin/speedtest
+chown root:root /usr/local/bin/speedtest
